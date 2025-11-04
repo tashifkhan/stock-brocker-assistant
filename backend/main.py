b745669a0025be1212cdd80d81e2757497fffeba
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-11-04 15:22:38 +0530

    structured backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from backend!")
-
-
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run(
+        "backend.main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_level="info",
+        reload=True,
+    )
---
+++ backend/routes/__init__.py
@@ -0,0 +1,3 @@
+"""
+all the api routers here
+"""
